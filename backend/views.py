91ac988ceb371614d183c6834aabc772b01db5f8
Muxalko <muxa.baba@gmail.com>
2023-12-15 02:39:55 +0200

    update ViewSets

--- backend/views.py
+++ backend/views.py
@@ -5,11 +5,12 @@
 # from django.contrib.auth.models import User
 
 # from django.core.serializers import serialize
-from backend.models import Place, Category
+from backend.models import Place, Category, Tag, Address, Request
 
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets, mixins, permissions
-from backend.serializers import UserSerializer, GroupSerializer, CategorySerializer, PlaceSerializer
+from backend.serializers import UserSerializer, GroupSerializer, \
+    CategorySerializer, PlaceSerializer, AddressSerializer, TagSerializer, RequestSerializer
 from rest_framework_gis.filters import InBBoxFilter, TMSTileFilter
 
 # #password = User.objects.make_random_password() # 7Gjk2kd4T9
@@ -55,32 +56,46 @@ class GroupViewSet(viewsets.ModelViewSet):
     
     from rest_framework import viewsets, mixins
 
-class PlaceViewSet(mixins.RetrieveModelMixin,
+class AddressViewSet(mixins.RetrieveModelMixin,
                     mixins.ListModelMixin,
                     viewsets.GenericViewSet):
     """
-    API endpoint that allows places to be viewed.
+    API endpoint that allows addresses to be viewed.
     """
-    queryset = Place.objects.all()
-    serializer_class = PlaceSerializer
+    queryset = Address.objects.all()
+    serializer_class = AddressSerializer
     # permission_classes = [permissions.IsAuthenticated]
     bbox_filter_field = 'location'
     
-    
     #TMSTileFilter: /?tile=8/100/200
-    filter_backends = (TMSTileFilter,)
+    # filter_backends = (TMSTileFilter,)
     #InBBoxFilter:  /?in_bbox=-90,29,-89,35
-    # filter_backends = (InBBoxFilter,)
+    filter_backends = (InBBoxFilter,)
     bbox_filter_include_overlapping = True # Optional
 
+class RequestViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows requests to be viewed.
+    """
+    queryset = Request.objects.all()
+    serializer_class = RequestSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
-# class LocationList(ListAPIView):
 
-#     queryset = Place.objects.all()
-#     serializer_class = PlaceSerializer
-#     bbox_filter_field = 'point'
-#     filter_backends = (InBBoxFilter,)
-#     bbox_filter_include_overlapping = True # Optional
+class PlaceViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows addresses to be viewed.
+    """
+    queryset = Address.objects.all()
+    serializer_class = AddressSerializer
+    # permission_classes = [permissions.IsAuthenticated]
+    bbox_filter_field = 'location'
+    
+    #TMSTileFilter: /?tile=8/100/200
+    # filter_backends = (TMSTileFilter,)
+    #InBBoxFilter:  /?in_bbox=-90,29,-89,35
+    filter_backends = (InBBoxFilter,)
+    bbox_filter_include_overlapping = True # Optional
 
 class CategoryViewSet(viewsets.ModelViewSet):
     """
@@ -89,5 +104,11 @@ class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
     permission_classes = [permissions.IsAuthenticated]
-    
-    from rest_framework import viewsets, mixins
+
+class TagViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows categories to be viewed or edited.
+    """
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+    # permission_classes = [permissions.IsAuthenticated]
