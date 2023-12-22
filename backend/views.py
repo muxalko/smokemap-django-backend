c999911fc5f85bc54f5998750aa0dd1ea51beee8
muxalko <muxa.baba@gmail.com>
2023-12-22 11:33:23 -0800

    Development (#11)
    * real db
    
    * add real deb and new secret
    
    * update settings
    
    * fix statics
    
    * add empty static dir
    
    * Feature/recent changes (#2)
    
    * add graphene support
    
    * postgis + graphql +cors
    
    * prepare data models + schema
    
    * custom vercel runtime for geospatial libraries
    
    * add debug PATH log
    
    * just to trigger build
    
    * remove unnecessary log
    
    * fix json
    
    * test install spatial lib deps
    
    * remove gdal setup
    
    * try custom runtime
    
    * rm rewrites
    
    * change path
    
    * fix typo
    
    * revert back the changes
    
    * save spacial deps in repo
    
    * rm binaries
    
    * debug env
    
    * use custom builder
    
    * fix path
    
    * set proper env for GDAL deps
    
    * env changes
    
    * fix cors for preview
    
    * add cors allowed origins
    
    * Feature/recent changes (#6)
    
    * add graphene support
    
    * postgis + graphql +cors
    
    * prepare data models + schema
    
    * custom vercel runtime for geospatial libraries
    
    * add debug PATH log
    
    * just to trigger build
    
    * remove unnecessary log
    
    * fix json
    
    * test install spatial lib deps
    
    * remove gdal setup
    
    * try custom runtime
    
    * rm rewrites
    
    * change path
    
    * fix typo
    
    * revert back the changes
    
    * save spacial deps in repo
    
    * rm binaries
    
    * debug env
    
    * use custom builder
    
    * fix path
    
    * set proper env for GDAL deps
    
    * env changes
    
    * fix cors for preview
    
    * add cors allowed origins
    
    * Hotfix/typo (#8)
    
    * add graphene support
    
    * postgis + graphql +cors
    
    * prepare data models + schema
    
    * custom vercel runtime for geospatial libraries
    
    * add debug PATH log
    
    * just to trigger build
    
    * remove unnecessary log
    
    * fix json
    
    * test install spatial lib deps
    
    * remove gdal setup
    
    * try custom runtime
    
    * rm rewrites
    
    * change path
    
    * fix typo
    
    * revert back the changes
    
    * save spacial deps in repo
    
    * rm binaries
    
    * debug env
    
    * use custom builder
    
    * fix path
    
    * set proper env for GDAL deps
    
    * env changes
    
    * fix cors for preview
    
    * add cors allowed origins
    
    * add missing comma
    
    * Feature/recent changes (#10)
    
    * add graphene support
    
    * postgis + graphql +cors
    
    * prepare data models + schema
    
    * custom vercel runtime for geospatial libraries
    
    * add debug PATH log
    
    * just to trigger build
    
    * remove unnecessary log
    
    * fix json
    
    * test install spatial lib deps
    
    * remove gdal setup
    
    * try custom runtime
    
    * rm rewrites
    
    * change path
    
    * fix typo
    
    * revert back the changes
    
    * save spacial deps in repo
    
    * rm binaries
    
    * debug env
    
    * use custom builder
    
    * fix path
    
    * set proper env for GDAL deps
    
    * env changes
    
    * fix cors for preview
    
    * add cors allowed origins
    
    * add missing comma
    
    * fixing models + rest + serializers + geojson
    
    * rework for geojson
    
    * update settings
    
    * fix
    
    * add model changes
    
    * reset migration
    
    * bump deps
    
    * add django-filter
    
    * update models
    
    * update schema
    
    * update views
    
    * admin view fix
    
    * update serializers
    
    * update ViewSets
    
    * update serializers
    
    * updated with recent changes
    
    * fix field name
    
    * set search_path for initial migrate
    
    * ignore env files
    
    * add initial migration
    
    * fix cors
    
    * set regex for cors
    
    * fix cors
    
    * add cors for preview
    
    * fix cors
    
    * add node_modules to gitignore
    
    * fix typo
    
    * fix typo

--- backend/views.py
+++ backend/views.py
@@ -1,16 +1,122 @@
 # backend/views.py
-from datetime import datetime
+# from datetime import datetime
 
-from django.http import HttpResponse
+# from django.http import HttpResponse
+# from django.contrib.auth.models import User
 
-def index(request):
-    now = datetime.now()
-    html = f'''
-    <html>
-        <body>
-            <h1>Hello from Vercel!</h1>
-            <p>The current time is { now }.</p>
-        </body>
-    </html>
-    '''
-    return HttpResponse(html)
+# from django.core.serializers import serialize
+from backend.models import Place, Category, Tag, Address, Request
+
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets, mixins, permissions
+from backend.serializers import UserSerializer, GroupSerializer, \
+    CategorySerializer, PlaceSerializer, AddressSerializer, TagSerializer, RequestSerializer
+from rest_framework_gis.filters import InBBoxFilter, TMSTileFilter
+
+# #password = User.objects.make_random_password() # 7Gjk2kd4T9
+# #password = User.objects.make_random_password(length=14) # FTELhrNFdRbSgy
+# password = User.objects.make_random_password(length=14, allowed_chars='!#$%&()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[]^_abcdefghijklmnopqrstuvwxyz{|}~') # zvk0hawf8m6394
+
+# #user.set_password(password)
+
+# def index(request):
+#     now = datetime.now()
+#     geojson = serialize("geojson", Place.objects.all(), geometry_field="location", fields=["name"])
+
+#     html = f'''
+#     <html>
+#         <body>
+#             <h1>Hello from Vercel!</h1>
+#             <p>The current time is { now }.</p>
+#             <p>The generated password is  { password }</p>
+#             { geojson }
+#         </body>
+#     </html>
+#     '''
+#     return HttpResponse(html)
+
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    
+    from rest_framework import viewsets, mixins
+
+class AddressViewSet(mixins.RetrieveModelMixin,
+                    mixins.ListModelMixin,
+                    viewsets.GenericViewSet):
+    """
+    API endpoint that allows addresses to be viewed.
+    """
+    queryset = Address.objects.all()
+    serializer_class = AddressSerializer
+    # permission_classes = [permissions.IsAuthenticated]
+    bbox_filter_field = 'location'
+    
+    #TMSTileFilter: /?tile=8/100/200
+    # filter_backends = (TMSTileFilter,)
+    #InBBoxFilter:  /?in_bbox=-90,29,-89,35
+    filter_backends = (InBBoxFilter,)
+    bbox_filter_include_overlapping = True # Optional
+
+class RequestViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows requests to be viewed.
+    """
+    queryset = Request.objects.all()
+    serializer_class = RequestSerializer
+    # permission_classes = [permissions.IsAuthenticated]
+
+
+class PlaceViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows places to be viewed.
+    """
+    queryset = Place.objects.all()
+    serializer_class = PlaceSerializer
+    # permission_classes = [permissions.IsAuthenticated]
+    # bbox_filter_field = 'address.location'
+    
+    #TMSTileFilter: /?tile=8/100/200
+    # filter_backends = (TMSTileFilter,)
+    #InBBoxFilter:  /?in_bbox=-90,29,-89,35
+    # filter_backends = (InBBoxFilter,)
+    # bbox_filter_include_overlapping = True # Optional
+
+# class LocationList(ListAPIView):
+
+#     queryset = Place.objects.all()
+#     serializer_class = PlaceSerializer
+#     bbox_filter_field = 'point'
+#     filter_backends = (InBBoxFilter,)
+#     bbox_filter_include_overlapping = True # Optional
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows categories to be viewed or edited.
+    """
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class TagViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows categories to be viewed or edited.
+    """
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+    # permission_classes = [permissions.IsAuthenticated]
---
+++ build_files.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# IMPORTANT NOTE: after using the below section , rotate POSTGRES_PASSWORD, DJANGO_SECRET_KEY 
+# echo "DEBUG ENVIRONMENT"
+# echo ""
+# echo "printenv"
+# printenv
+# echo ""
+# echo "ls"
+# ls -ltrah
+# echo ""
+# echo "pwd"
+# pwd
+# echo ""
+# echo "find libgdal.so"
+# find / -name libgdal.so*
+# echo ""
+# echo "find libgdal.so"
+# find / -name libgeos_c.so.1*
+# echo ""
+# echo "-----------"
+#find / -name libproj.so.22
+echo "BUILD START"
+
+# set location for GDAL dependency files
+export LD_LIBRARY_PATH="$(pwd)/.vercel/builders/node_modules/vercel-python-gis/dist/files/"
+
+# Create a virtual environment
+echo "Creating a virtual environment..."
+python3.9 -m venv venv
+source venv/bin/activate
+
+# Install the latest version of pip
+echo "Installing the latest version of pip..."
+python -m pip install --upgrade pip
+
+# Build the project
+echo "Building the project..."
+python -m pip install -r requirements.txt
+
+# Make migrations
+echo "Making migrations..."
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
+
+# Collect static files
+echo "Collecting static files..."
+python manage.py collectstatic --noinput --clear
+
+
+echo "BUILD END"
