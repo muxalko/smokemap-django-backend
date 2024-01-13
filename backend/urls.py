a431f60beb17bdd9d224b0ef859bfd969cac54f9
Muxalko <muxa.baba@gmail.com>
2024-01-13 07:18:12 +0200

    expose only places api

--- backend/urls.py
+++ backend/urls.py
@@ -13,14 +13,14 @@ from rest_framework import routers
 from backend import views
 
 router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
-router.register(r'categories', views.CategoryViewSet)
-router.register(r'tags', views.TagViewSet)
+# router.register(r'users', views.UserViewSet)
+# router.register(r'groups', views.GroupViewSet)
+# router.register(r'categories', views.CategoryViewSet)
+# router.register(r'tags', views.TagViewSet)
+# router.register(r'addresses', views.AddressViewSet)
+# router.register(r'requests', views.RequestViewSet)
+# router.register(r'images', views.ImageViewSet)
 router.register(r'places', views.PlaceViewSet)
-router.register(r'addresses', views.AddressViewSet)
-router.register(r'requests', views.RequestViewSet)
-router.register(r'images', views.ImageViewSet)
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
