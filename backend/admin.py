fbb650361255614782b3fdd17307b063d795ed68
Muxalko <muxa.baba@gmail.com>
2024-01-13 07:17:55 +0200

    add Image to admin

--- backend/admin.py
+++ backend/admin.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
 from django.contrib.gis.admin import OSMGeoAdmin
 
-from .models import Category, Tag, Address, Request, Place
+from .models import Category, Tag, Address, Request, Place, Image
 # Register your models here.
 admin.site.register(Category)
 admin.site.register(Tag)
 admin.site.register(Address)
 admin.site.register(Request)
-
+admin.site.register(Image)
 @admin.register(Place)
 class PlaceAdmin(OSMGeoAdmin):
     list_display = ('name', 'address')
