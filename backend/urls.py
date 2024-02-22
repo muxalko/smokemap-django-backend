fd49919150bf61da00b092e77980e7002905eff3
Muxalko <muxa.baba@gmail.com>
2024-02-22 19:54:52 +0200

    remove graphiql in prod

--- backend/urls.py
+++ backend/urls.py
@@ -4,6 +4,7 @@ from rest_framework import routers
 from backend import views
 from django.views.decorators.csrf import csrf_exempt
 from graphene_django.views import GraphQLView
+from django.conf import settings
 
 router = routers.DefaultRouter()
 
@@ -13,5 +14,5 @@ router.register(r'places', views.PlaceViewSet)
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('', include(router.urls)),
-    path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True))),
+    path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=settings.DEBUG))),
 ]
